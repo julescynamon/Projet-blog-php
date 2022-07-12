9d065f4cb5ffd4cf3438b462503d379144036c19
cynamon jules <julescweb@mail.com>
2022-07-12 16:36:33 +0200

    affichage des articles

--- add-article.php
+++ add-article.php
@@ -1,38 +1,37 @@
 <?php
 
-const ERROR_REQUIRED = 'Ce champ est requis';
+const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
 const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
 const ERROR_CONTENT_TOO_SHORT = 'L\'article est trop court';
-const ERROR_IMAGE_URL_INVALID = 'L\'URL de l\'image est invalide';
+const ERROR_IMAGE_URL = 'L\'image doit être une URL valide';
 
-$filename = __DIR__ . 'data/articles.json';
+$filename = __DIR__ . '/data/articles.json';
 
 $errors = [
 	'title' => '',
 	'image' => '',
 	'category' => '',
-	'content' => ''
+	'content' => '',
 ];
 
-$articles = [];
+if (file_exists($filename)) {
+	$articles = json_decode(
+		file_get_contents($filename),
+		true
+	) ?? [];
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-	if (file_exists($filename)) {
-		$articles = json_decode(file_get_contents($filename), true) ?? [];
-	}
-
-
 	$_POST = filter_input_array(INPUT_POST, [
-		'title' => FILTER_UNSAFE_RAW,
+		'title' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
 		'image' => FILTER_SANITIZE_URL,
-		'category' => FILTER_UNSAFE_RAW,
+		'category' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
 		'content' => [
-			'filter' => FILTER_UNSAFE_RAW,
+			'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
 			'flags' => FILTER_FLAG_NO_ENCODE_QUOTES
 		]
 	]);
-
 	$title = $_POST['title'] ?? '';
 	$image = $_POST['image'] ?? '';
 	$category = $_POST['category'] ?? '';
@@ -47,45 +46,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if (!$image) {
 		$errors['image'] = ERROR_REQUIRED;
 	} elseif (!filter_var($image, FILTER_VALIDATE_URL)) {
-		$errors['image'] = ERROR_IMAGE_URL_INVALID;
+		$errors['image'] = ERROR_IMAGE_URL;
 	}
 
 	if (!$category) {
 		$errors['category'] = ERROR_REQUIRED;
 	}
-	
+
 	if (!$content) {
 		$errors['content'] = ERROR_REQUIRED;
 	} elseif (mb_strlen($content) < 50) {
 		$errors['content'] = ERROR_CONTENT_TOO_SHORT;
 	}
 
-	if(empty(array_filter($errors, fn($e) => $e !== ''))) {
+	if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
 		$articles = [...$articles, [
 			'title' => $title,
 			'image' => $image,
 			'category' => $category,
-			'content' => $content
+			'content' => $content,
+			'id' => time()
 		]];
-
 		file_put_contents($filename, json_encode($articles));
 		header('Location: /');
 	}
 }
 
-
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-	<?php include('includes/head.php'); ?>
-	<link rel="stylesheet" href="public/css/add-article.css">
-	<title>Créez un article</title>
+	<?php require_once 'includes/head.php' ?>
+	<link rel="stylesheet" href="/public/css/add-article.css">
+
+	<title>Créer un article</title>
 </head>
 
 <body>
@@ -93,20 +89,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		<?php require_once 'includes/header.php' ?>
 		<div class="content">
 			<div class="block p-20 form-container">
-				<h1>Écrire un article</h1>
-				<form class="form-article" action="/add-article.php" method="POST">
+				<h1>Ecrire un article</h1>
+				<form action="/add-article.php" method="post">
 					<div class="form-control">
 						<label for="title">Titre</label>
-						<input type="text" name="title" id="title">
-						<?php if($errors['title']): ?>
-						<p class="text-error"><?= $errors['title'] ?></p>
+						<input type="text" name="title" id="title" value=<?= $title ?? '' ?>>
+						<?php if ($errors['title']) : ?>
+							<p class="error"><?= $errors['title'] ?></p>
 						<?php endif; ?>
 					</div>
 					<div class="form-control">
 						<label for="image">Image</label>
-						<input type="text" name="image" id="image">
-						<?php if($errors['image']): ?>
-						<p class="text-error"><?= $errors['image'] ?></p>
+						<input type="text" name="image" id="image" value=<?= $image ?? '' ?>>
+						<?php if ($errors['image']) : ?>
+							<p class="error"><?= $errors['image'] ?></p>
 						<?php endif; ?>
 					</div>
 					<div class="form-control">
@@ -114,21 +110,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 						<select name="category" id="category">
 							<option value="technology">Technologie</option>
 							<option value="nature">Nature</option>
-							<option value="politic">Politique</option>
+							<option value="politics">Politique</option>
 						</select>
-						<?php if($errors['category']): ?>
-						<p class="text-error"><?= $errors['category'] ?></p>
+						<?php if ($errors['category']) : ?>
+							<p class="error"><?= $errors['category'] ?></p>
 						<?php endif; ?>
 					</div>
 					<div class="form-control">
-						<label for="content">Contenue</label>
+						<label for="content">Contenu</label>
 						<textarea name="content" id="content"></textarea>
-						<?php if($errors['content']): ?>
-						<p class="text-error"><?= $errors['content'] ?></p>
+						<?php if ($errors['content']) : ?>
+							<p class="error"><?= $errors['content'] ?></p>
 						<?php endif; ?>
 					</div>
 					<div class="form-actions">
-						<a href="/" class="btn btn-secondary" type="button">Annuler</a>
+						<button class="btn btn-secondary" type="button">Annuler</button>
 						<button class="btn btn-primary" type="submit">Sauvegarder</button>
 					</div>
 				</form>
@@ -136,6 +132,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		</div>
 		<?php require_once 'includes/footer.php' ?>
 	</div>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
 
+if (file_exists($filename)) {
+	$articles = json_decode(
+		file_get_contents($filename),
+		true
+	) ?? [];
+}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-	<?php include('includes/head.php'); ?>
-	<link rel="stylesheet" href="public/css/index.css">
+	<?php require_once 'includes/head.php' ?>
+	<link rel="stylesheet" href="/public/css/index.css">
 	<title>Blog</title>
 </head>
 
@@ -16,11 +23,20 @@
 	<div class="container">
 		<?php require_once 'includes/header.php' ?>
 		<div class="content">
-
-
+			<div class="articles-container">
+				<?php foreach ($articles as $a) : ?>
+					<div class="article block">
+						<div class="overflow-img">
+							<div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+						</div>
+						<h2><?= $a['title'] ?></h2>
+					</div>
+				<?php endforeach; ?>
+			</div>
 		</div>
 		<?php require_once 'includes/footer.php' ?>
 	</div>
+
 </body>
 
 </html>
