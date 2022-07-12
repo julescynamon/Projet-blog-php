e9e82cada397be7a609dda67179f0f6dd145c2e5
cynamon jules <julescweb@mail.com>
2022-07-12 19:01:38 +0200

    mise en place du filtre de recherche

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?php
 $filename = __DIR__ . '/data/articles.json';
 $articles = [];
+$category = [];
 
 if (file_exists($filename)) {
 	$articles = json_decode(
 		file_get_contents($filename),
 		true
 	) ?? [];
+
+	$cattmp = array_map(fn($a) => $a['category'], $articles);
+	$category = array_reduce($cattmp, function($acc, $cat) {
+		if (!isset($acc[$cat])){
+			$acc[$cat]++;
+		} else {
+			$acc[$cat] = 1;
+		}
+		return $acc;
+	} ,[]);
+
+	$articlesPerCategories = array_reduce($articles, function($acc, $article){
+		if(isset($acc[$article['category']])){
+			$acc[$article['category']] = [...$acc[$article['category']], $article];
+		} else {
+			$acc[$article['category']] = [$article];
+		}
+	},[]); 
+
+	
 }
+
+
+
 ?>
 
 <!DOCTYPE html>
